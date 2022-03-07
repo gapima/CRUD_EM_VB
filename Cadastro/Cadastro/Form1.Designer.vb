aabada9c4cdfe04d56c6e65724186e8e4717d4f3
gapima <gapima7@gmail.com>
2022-03-07 14:10:11 -0300

    Crud em Visual basic

---
+++ Cadastro/Cadastro/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Cadastro
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.txtEndereco = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.btnNovo = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.BtnExcluir = New System.Windows.Forms.Button()
+        Me.btnExibir = New System.Windows.Forms.Button()
+        Me.btnConsultar = New System.Windows.Forms.Button()
+        Me.dgvDados = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nome:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Numero:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Endereço:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 167)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Email:"
+        '
+        'txtNome
+        '
+        Me.txtNome.AccessibleDescription = "txtNome"
+        Me.txtNome.AccessibleName = "txtNome"
+        Me.txtNome.Location = New System.Drawing.Point(256, 55)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(257, 20)
+        Me.txtNome.TabIndex = 6
+        '
+        'txtNumero
+        '
+        Me.txtNumero.AccessibleDescription = "txtNumero"
+        Me.txtNumero.AccessibleName = "txtNumero"
+        Me.txtNumero.Location = New System.Drawing.Point(256, 93)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(155, 20)
+        Me.txtNumero.TabIndex = 7
+        Me.txtNumero.Tag = "txt"
+        '
+        'txtEndereco
+        '
+        Me.txtEndereco.AccessibleDescription = "txtEndereco"
+        Me.txtEndereco.AccessibleName = "txtEndereco"
+        Me.txtEndereco.Location = New System.Drawing.Point(256, 132)
+        Me.txtEndereco.Name = "txtEndereco"
+        Me.txtEndereco.Size = New System.Drawing.Size(257, 20)
+        Me.txtEndereco.TabIndex = 8
+        '
+        'txtEmail
+        '
+        Me.txtEmail.AccessibleDescription = "txtEmail"
+        Me.txtEmail.AccessibleName = "txtEmail"
+        Me.txtEmail.Location = New System.Drawing.Point(256, 167)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(257, 20)
+        Me.txtEmail.TabIndex = 9
+        '
+        'btnNovo
+        '
+        Me.btnNovo.Location = New System.Drawing.Point(93, 217)
+        Me.btnNovo.Name = "btnNovo"
+        Me.btnNovo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNovo.TabIndex = 13
+        Me.btnNovo.Text = "Novo"
+        Me.btnNovo.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(200, 217)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEditar.TabIndex = 14
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'BtnExcluir
+        '
+        Me.BtnExcluir.Location = New System.Drawing.Point(313, 217)
+        Me.BtnExcluir.Name = "BtnExcluir"
+        Me.BtnExcluir.Size = New System.Drawing.Size(75, 23)
+        Me.BtnExcluir.TabIndex = 15
+        Me.BtnExcluir.Text = "Excluir"
+        Me.BtnExcluir.UseVisualStyleBackColor = True
+        '
+        'btnExibir
+        '
+        Me.btnExibir.Location = New System.Drawing.Point(417, 217)
+        Me.btnExibir.Name = "btnExibir"
+        Me.btnExibir.Size = New System.Drawing.Size(75, 23)
+        Me.btnExibir.TabIndex = 16
+        Me.btnExibir.Text = "Exibir"
+        Me.btnExibir.UseVisualStyleBackColor = True
+        '
+        'btnConsultar
+        '
+        Me.btnConsultar.Location = New System.Drawing.Point(523, 217)
+        Me.btnConsultar.Name = "btnConsultar"
+        Me.btnConsultar.Size = New System.Drawing.Size(75, 23)
+        Me.btnConsultar.TabIndex = 17
+        Me.btnConsultar.Text = "Consultar"
+        Me.btnConsultar.UseVisualStyleBackColor = True
+        '
+        'dgvDados
+        '
+        Me.dgvDados.AccessibleDescription = "dgvDados"
+        Me.dgvDados.AccessibleName = "dgvDados"
+        Me.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDados.Location = New System.Drawing.Point(26, 264)
+        Me.dgvDados.Name = "dgvDados"
+        Me.dgvDados.Size = New System.Drawing.Size(732, 204)
+        Me.dgvDados.TabIndex = 18
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 28)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(18, 13)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "ID"
+        '
+        'txtID
+        '
+        Me.txtID.AccessibleDescription = "txtID"
+        Me.txtID.AccessibleName = "txtID"
+        Me.txtID.Location = New System.Drawing.Point(256, 21)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(43, 20)
+        Me.txtID.TabIndex = 20
+        Me.txtID.Tag = "txtID"
+        '
+        'Cadastro
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(807, 526)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.dgvDados)
+        Me.Controls.Add(Me.btnConsultar)
+        Me.Controls.Add(Me.btnExibir)
+        Me.Controls.Add(Me.BtnExcluir)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnNovo)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtEndereco)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Cadastro"
+        Me.Text = "Cadastro"
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtNome As TextBox
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents txtEndereco As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents btnNovo As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents BtnExcluir As Button
+    Friend WithEvents btnExibir As Button
+    Friend WithEvents btnConsultar As Button
+    Friend WithEvents dgvDados As DataGridView
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtID As TextBox
+End Class
---
+++ Cadastro/Cadastro/Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class Cadastro
+    Private conexao As SqlConnection
+    Private comando As SqlCommand
+    Private da As SqlDataAdapter
+    Private dr As SqlDataReader
+    Private strSQL As String
+
+
+    Private Sub btnNovo_Click(sender As Object, e As EventArgs) Handles btnNovo.Click
+
+
+        Try
+
+            conexao = New SqlConnection("Server=TL026;Database=BANCO DE TESTE;User Id=sa;Password=111@senha;
+")
+            strSQL = " INSERT INTO CAD_CLIENTE (NOME, NUMERO, ENDERECO, EMAIL) VALUES (@NOME, @NUMERO, @ENDERECO, @EMAIL)"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@NOME", txtNome.Text)
+            comando.Parameters.AddWithValue("@NUMERO", txtNumero.Text)
+            comando.Parameters.AddWithValue("@ENDERECO", txtEndereco.Text)
+            comando.Parameters.AddWithValue("@EMAIL", txtEmail.Text)
+
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+
+        End Try
+
+
+    End Sub
+
+    Private Sub btnExibir_Click(sender As Object, e As EventArgs) Handles btnExibir.Click
+
+
+        Try
+
+            conexao = New SqlConnection("Server=nomedoserver;Database=bomedobanco DE TESTE;User Id=sa;Password=senhabanco;")
+            strSQL = "SELECT * FROM CAD_CLIENTE"
+
+            comando = New SqlCommand(strSQL, conexao)
+            da = New SqlDataAdapter(strSQL, conexao)
+            Dim ds As New DataSet
+            da.Fill(ds)
+
+            dgvDados.DataSource = ds.Tables(0)
+            conexao.Open()
+
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+
+        End Try
+    End Sub
+
+    Private Sub btnConsultar_Click(sender As Object, e As EventArgs) Handles btnConsultar.Click
+        Try
+
+            conexao = New SqlConnection("Server=nomedoserver;Database=bomedobanco DE TESTE;User Id=sa;Password=senhabanco;")
+            strSQL = "SELECT * FROM CAD_CLIENTE WHERE ID = @ID"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@ID", txtID.Text)
+            conexao.Open()
+
+            dr = comando.ExecuteReader
+
+            Do While dr.Read
+                txtNome.Text = dr("nome")
+                txtNumero.Text = dr("numero")
+                txtEndereco.Text = dr("endereco")
+                txtEmail.Text = dr("email")
+
+            Loop
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+            dr = Nothing
+
+        End Try
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        Try
+
+            conexao = New SqlConnection("Server=nomedoserver;Database=bomedobanco DE TESTE;User Id=sa;Password=senhabanco;")
+            strSQL = " UPDATE CAD_CLIENTE SET NOME = @NOME, NUMERO = @NUMERO, ENDERECO = @ENDERECO, EMAIL = @EMAIL WHERE ID = @ID"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@ID", txtID.Text)
+            comando.Parameters.AddWithValue("@NOME", txtNome.Text)
+            comando.Parameters.AddWithValue("@NUMERO", txtNumero.Text)
+            comando.Parameters.AddWithValue("@ENDERECO", txtEndereco.Text)
+            comando.Parameters.AddWithValue("@EMAIL", txtEmail.Text)
+
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+
+        End Try
+    End Sub
+
+    Private Sub BtnExcluir_Click(sender As Object, e As EventArgs) Handles BtnExcluir.Click
+        Try
+
+            conexao = New SqlConnection("Server=nomedoserver;Database=bomedobanco DE TESTE;User Id=sa;Password=senhabanco;")
+            strSQL = " DELETE CAD_CLIENTE WHERE ID = @ID"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@ID", txtID.Text)
+
+
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+
+        End Try
+    End Sub
+End Class
+
+
